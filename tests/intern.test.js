b30d2abc5f428acc4ca0661c5d2a9bb99577983a
unknown <thecbradshaw89@gmail.com>
2023-01-30 18:20:46 -0600

    added test for intern: passes. adding test functionality for other classes.

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,4 +1,24 @@
 //test object creation, is an instance of that class,
 
 //test methods within it 
-const Employee = require('./')
+const Employee = require('../lib/employee');
+const Intern = require('../lib/intern');
+
+
+
+describe('Intern', () => {
+    describe('Initialization', () => {
+        it("Should create a new Intern object if provided valid arguments", () => {
+            
+            const intern = new Intern("Derek", 1, "the@mail.com", "Lincoln Center");
+
+            expect(intern.name).toEqual("Derek");
+            expect(intern.id).toEqual(1);
+            expect(intern.email).toEqual("the@mail.com");
+            expect(intern.school).toEqual("Lincoln Center");
+        });
+    })
+})
+
+//styling: bootstrap
+//do other tests for each 
